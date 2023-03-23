fa7c5bea2d3dffa52478188abaf79e0dbb810ef3
Davinchiam1 <ruslananushko2113@gmail.com>
2023-03-23 10:19:44 +0300

    add stable version of markers(read from txt)

--- mpstat_data.py
+++ mpstat_data.py
@@ -5,6 +5,7 @@ import os
 
 # Получение данных в формате выгрузки Mpstat и их совмещение по месяцам с добавлением метки времени
 def marker(row, colum, keywords):
+    """" Base func for marking rows in frame by the presence of keywords"""
     val = 0
     for keyword in keywords:
         if row[colum].find(keyword) ==-1:
@@ -46,19 +47,34 @@ class Mpstat_data:
         else:
             self.final_frame = pd.concat([self.temp_frame, self.final_frame], sort=False, axis=0)
 
-    def _get_makers(self, colum, markers={}):
+    def _get_makers(self, colum, markers_file='./markers.txt'):
+        markers = {}
+        with open(markers_file, "r", encoding='utf8') as file:
+            for line in file:
+                temp_line = line.split(':')
+                key = temp_line[0][0:-1] if temp_line[0][-1] == ' ' else temp_line[0]
+                keywords_temp = temp_line[1].split(',')
+                keywords = []
+                for keyword in keywords_temp:
+                    if keyword[0] == ' ' and keyword[-1] != '\n':
+                        keyword = keyword[1:]
+                    elif keyword[-1] == '\n' and keyword[0] == ' ':
+                        keyword = keyword[1:-1]
+                    keywords.append(keyword)
+                markers[key] = keywords
         for key in markers:
             self.final_frame[key] = self.final_frame.apply(lambda row: marker(row=row, colum=colum, keywords=markers[key]), axis=1)
 
 
-    def use_script(self, finalname='./final.xlsx', set_dates=True):
+    def use_script(self, finalname='./final.xlsx', set_dates=True, markers_file=None, colum=None):
         self._create_lists()
         self.set_dates = set_dates
 
         for filename, date in zip(self.file_list, self.date_list):
             self._get_data(filename=filename, date=date)
             self._concentrate_data()
-        self._get_makers(colum='Название', markers={'test': ['увлаж', 'коллаг'],'test2':['омолаж'],'test3':['Крем']})
+        if markers_file is not None:
+            self._get_makers(colum=colum, markers={'test': ['увлаж', 'коллаг'],'test2':['омолаж'],'test3':['Крем']})
         self.final_frame.head(10)
         self.final_frame.to_excel(finalname, sheet_name='list1', index=False)
 
